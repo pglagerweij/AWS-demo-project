a4d56e32f635013b613346c3a83875087dba260e
Pascal Lagerweij <pascallagerweij@gmail.com>
2022-11-23 18:19:14 +0100

    adjust some mistakes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,24 @@
 variable "region" {
   description = "Default region of this terraform project"
+  type = string
 }
 
 variable "dynamo_table" {
   description = "Dynamo table name"
+  type = string
 }
 
 variable "state_bucket_name" {
   description = "Terraform state file bucket name"
+  type = string
 }
 
 variable "github_repo_name" {
   description = "Github repository name"
+  type = string
 }
 
 variable "github_org_name" {
   description = "Github organisation name"
+  type = string
 }
