8e4e089a06373804905cfa00cc798d4d4202f37e
Pascal Lagerweij <pascallagerweij@gmail.com>
2022-11-23 19:50:33 +0100

    add stuff

--- terraform/bootstrap.tf
+++ terraform/bootstrap.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "terraform_lock" {
   name         = var.dynamo_table
-  billing_mode   = "PAY_PER_REQUEST"
+  billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
   attribute {
