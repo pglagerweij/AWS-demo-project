09bed634355bef77509d942d384ed8a9b4048c39
Pascal Lagerweij <pascallagerweij@gmail.com>
2022-11-23 20:01:19 +0100

    update

--- terraform/tst.tf
+++ terraform/tst.tf
@@ -8,3 +8,8 @@ resource "aws_dynamodb_table" "tf_tst-testing" {
     type = "S"
   }
 }
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t1.2xlarge" # invalid type!
+}
