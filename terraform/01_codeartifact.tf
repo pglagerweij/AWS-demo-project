6d8c12e2b38d7539f02871fa977203a1a59111ef
Pascal Lagerweij <plagerweij@deloitte.nl>
2022-11-24 17:04:16 +0100

    add codeartifact role

--- terraform/01_codeartifact.tf
+++ terraform/01_codeartifact.tf
@@ -19,3 +19,49 @@ resource "aws_codeartifact_repository" "upstream" {
     external_connection_name = "public:pypi"
   }
 }
+
+module "github_readonly" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "~> 5.8"
+
+  create_role = true
+
+  role_name = "oidc_codeartifact_ro"
+
+  tags = {
+    Role = "role-codeartifact-with-oidc"
+  }
+
+  provider_url = "token.actions.githubusercontent.com"
+
+  role_policy_arns = [
+    aws_iam_policy.github_codeartifact.arn
+  ]
+
+  oidc_fully_qualified_subjects = ["repo:${var.github_org_name}/${var.github_repo_name}:pull_request"]
+}
+
+data "aws_iam_policy_document" "github_codeartifact" {
+  statement {
+    sid = "StateBucket"
+    actions = [
+      "codeartifact:GetAuthorizationToken",
+      "codeartifact:ReadFromRepository"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid = "Get token for codeartifact"
+    actions = [
+      "sts:GetServiceBearerToken",
+    ]
+    resources = ["*"]
+  }
+
+}
+
+resource "aws_iam_policy" "github_codeartifact" {
+  name        = "oidc_codeartifact_readonly"
+  description = "Permissions to access state bucket and dynamo table"
+  policy      = data.aws_iam_policy_document.github_readonly.json
+}
